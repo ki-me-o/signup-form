8abe64c9a358c5410fe521e84c3a51a231feeb36
Sebastian S <sebastian.subola@gmail.com>
2023-07-28 16:10:39 -0600

    Allow error checking to update both fields

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const password_input = document.querySelector('input[name="password"]');
 const verify_password_input = document.querySelector('input[name="confirm_password"]');
 
-function checkPasswords(e){    
+function checkPasswords(){    
     var password = password_input.value;
-    var passwordVerify = e.target.value;
+    var passwordVerify = verify_password_input.value;
     if(password !== passwordVerify) {
+        verify_password_input.removeAttribute("class", "match");
+        password_input.removeAttribute("class", "match");
         verify_password_input.setAttribute("class", "error");
+        password_input.setAttribute("class", "error");
+
+
     } else {
+        password_input.removeAttribute("class", "error");
         verify_password_input.removeAttribute("class", "error");
+        password_input.setAttribute("class", "match");
+        verify_password_input.setAttribute("class", "match");
     }
 }
 
+password_input.addEventListener("input", checkPasswords);
 verify_password_input.addEventListener("input", checkPasswords);
+
